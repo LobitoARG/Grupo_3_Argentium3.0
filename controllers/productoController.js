3c9a0f8ce4ecbc3529764f051d338403de1aa1fb
unknown <hjabo97@gmail.com>
2022-03-02 21:24:57 -0300

    Sumamos las nuevas vistas de crear y editar productos, tmb sumamos los controllers, etc

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -3,7 +3,9 @@ const express = require('express');
 const productoController = {
 
     detailProduct: (req, res) => res.render('./products/detailProduct'),
-    productCart: (req, res) => res.render('./products/productCart')
+    productCart: (req, res) => res.render('./products/productCart'),
+    createProduct: (req,res) => res.render('./products/createProduct'),
+    editProduct: (req,res) => res.render('./products/editProduct')
 
 }
 
--- routes/productoRoute.js
+++ routes/productoRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.get('/detailProduct', productoController.detailProduct);
 router.get('/productCart', productoController.productCart);
+router.get('/createProduct', productoController.createProduct);
+router.get('/editProduct', productoController.editProduct);
 
 
 module.exports = router; 
